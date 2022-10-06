818a5d61a39466ac0775fbc8ccccde280c0ceb2e
Brian Jensen <31598338+brian-jensen@users.noreply.github.com>
2022-10-06 06:18:49 -0700

    Add desktop HTML, CSS, & JS

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
-const footer = document.querySelector('footer');
-const aside = document.querySelector('footer aside');
-const button = document.querySelector('footer button');
+const footer = document.querySelector("footer");
+const aside = document.querySelector("footer aside");
+const button = document.querySelector("footer button");
+const tooltip = document.querySelector(".tooltip");
+const desktop = window.matchMedia("(min-width: 64em)");
+
 const original = aside.innerHTML;
 
-button.addEventListener('click', () => {
-  footer.classList.toggle('mobile-share');
-  footer.classList.add('flip');
-  aside.classList.toggle('share');
-  if (aside.classList.contains('share')) {
-    aside.classList.remove('flip')
-    aside.innerHTML = `
+button.addEventListener("click", () => {
+  const dynamic = `
     <h2>share</h2>
     <a href="https://www.facebook.com/" target="_blank">
       <img src="./images/icon-facebook.svg" alt="facebook icon">
@@ -21,10 +19,41 @@ button.addEventListener('click', () => {
       <img src="./images/icon-pinterest.svg" alt="pinterest icon">
     </a>
   `;
-  } else {
-    aside.innerHTML = original;
-    footer.classList.remove('flip');
-    aside.classList.add('flip')
-  }
 
+  if (desktop.matches) {
+    footer.classList.toggle("desktop-share");
+    tooltip.classList.toggle("share");
+    if (tooltip.classList.contains("share")) {
+      tooltip.style.display = "flex";
+      tooltip.innerHTML = dynamic;
+    } else {
+      tooltip.innerHTML = "";
+      tooltip.style.display = "";
+    }
+  } else {
+    footer.classList.toggle("mobile-share");
+    footer.classList.add("flip");
+    aside.classList.toggle("share");
+    if (aside.classList.contains("share")) {
+      aside.classList.remove("flip");
+      aside.innerHTML = dynamic;
+    } else {
+      aside.innerHTML = original;
+      footer.classList.remove("flip");
+      aside.classList.add("flip");
+    }
+  }
+});
+
+desktop.addEventListener("change", () => {
+  if (desktop.matches) {
+    footer.classList.remove("mobile-share", "flip");
+    aside.classList.remove("share", "flip");
+    aside.innerHTML = original;
+  } else {
+    footer.classList.remove("desktop-share");
+    tooltip.classList.remove("share");
+    tooltip.innerHTML = "";
+    tooltip.style.display = "";
+  }
 });
